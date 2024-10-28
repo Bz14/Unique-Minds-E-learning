8fcb083e0b5969e4804293d06b6194162c4509dc
Bz14 <bezueyerusalem@gmail.com>
2024-10-28 09:51:02 +0300

    Files updated.

--- Frontend/unique-minds/src/app/page.tsx
+++ Frontend/unique-minds/src/app/page.tsx
@@ -1,5 +1,5 @@
-import FeaturedCourseSection from "./components/Homepage/FeaturedCourseSection/featured_course_section";
-import HeroSection from "./components/Homepage/HeroSection/hero_section";
+import FeaturedCourseSection from "@/app/components/Homepage/FeaturedCourseSection/featured_course_section";
+import HeroSection from "@/app/components/Homepage/HeroSection/hero_section";
 
 const Home = () => {
   return (
