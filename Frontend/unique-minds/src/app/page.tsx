7590fab13d6a16b574bd23bfc7cf56b3db0028f1
Bz14 <bezueyerusalem@gmail.com>
2024-11-26 23:40:01 +0300

    Call to action section added.

--- Frontend/unique-minds/src/app/page.tsx
+++ Frontend/unique-minds/src/app/page.tsx
@@ -3,6 +3,8 @@
 import FeaturedCourseSection from "@/app/components/Homepage/FeaturedCourseSection/featured_course_section";
 import HeroSection from "@/app/components/Homepage/HeroSection/hero_section";
 import BenefitsSection from "./components/Homepage/Benefits_section/benifts";
+import TopEducators from "./components/Homepage/Top_educators/top_educator";
+import CallToAction from "./components/Homepage/Call_to_action/call_action";
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
       <HeroSection />
       <FeaturedCourseSection />
       <BenefitsSection />
+      <TopEducators />
+      <CallToAction />
     </>
   );
 };
